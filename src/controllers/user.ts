06c5e29ce620e7318f8da82d46f0871c002f6f53
Allanvini <allanvinisilva@gmail.com>
2021-08-30 11:05:46 -0300

    removido os comentários no codigo

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -4,8 +4,12 @@ const users: Object = [
     { id: 1, name: 'allan' }, { id: 2, name: 'joão' }, { id: 3, name: 'ercio'}
 ]
 
-export default class UserController{    
-    getUsers(request: Request, response: Response){
+const userController = {
+    
+    getUsers(request: Request, response: Response): Response{
         return response.status(200).send(users);
     }
+
 }
+
+export default userController;
--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,7 @@
 import { Router } from "express";
 const routes = Router();
+import userController from './controllers/user';
 
-import UserController from "./controllers/user";
-
-const userController = new UserController();
-
-const test: UserController = new UserController(); // Aqui criamos um tipo de variavel conforme a classe (igual em linguagens fortemente tipadas)
-
-routes.get('/', userController.getUsers)
+routes.get('/', userController.getUsers);
 
 export default routes;
